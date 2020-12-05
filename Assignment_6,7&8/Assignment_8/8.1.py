1dd3e51c20be62dd68466304a7b59f7a462fac8f
comattose <48693718+comattose@users.noreply.github.com>
2020-12-05 05:22:57 -0600

    Add files via upload

--- Assignment_6,7&8/Assignment_8/8.1.py
+++ Assignment_6,7&8/Assignment_8/8.1.py
@@ -67,22 +67,53 @@ for i in range(1,T):
 #print(values[T-1])  #test
 #print(probabilities) #test
 
-probabilities[0] = 1     #For whatever reason, this indexed value was showing up as zero before so I'm just manually making it 1 here.
+#probabilities[0] = 1     #For whatever reason, this indexed value was showing up as zero before so I'm just manually making it 1 here. Fixed.
 probabilities[:] = [x/total for x in probabilities] #Here I divide each value by total sum of probabilities to normalize each probability. 
 
 
-print('Random integer:',weighted_values(values, probabilities, 1))
+print('Random integer between 1 and 500:',weighted_values(values, probabilities, 1))
 
-print('For a random continuous outcome with weight p(x)=x^-1:')
+#print('For a random continuous outcome with weight p(x)=x^-1:')
 
-integral = integrate.quad(lambda x: x**-1,1,T)
-print(integrate.quad(lambda x: x**-1,1,T))
-r = random.uniform(0,1)
-integral = r
+#integral = integrate.quad(lambda x: x**-1,1,T)
+#print(integrate.quad(lambda x: x**-1,1,T))
+#r = random.uniform(0,1)
+
+#I think I see how to create expected distribution from this but not how to generate 10^5 numbers and plot that distribution.
+#You just have to set r = integral and then solve in terms of x.
 
 #For T = 10, generate 10^5 numbers using this program and plot the distribution
+print('For T = 10, distribution is as follows:')
+
+#def f():
+#    T = 10
+#    values = np.zeros(T) #Setting this up as a function since we're doing it 10^5 times.
+#    probabilities = np.zeros(T)
+#    total = 0
+#    
+#    for i in range(1,T):
+#        values[i] = i+1
+#        probabilities[i]=(i+1)**-1
+#        total += probabilities[i]
+#        
+#    probabilities[0] = 1
+#    probabilities[:] = [x/total for x in probabilities]
+#    return(weighted_values(values, probabilities, 1))
+
+T = 10
+values = np.zeros(T) #Setting this up as a function since we're doing it 10^5 times.
+probabilities = np.zeros(T)
+total = 0
+for i in range(1,T):
+    values[i] = i+1
+    probabilities[i]=(i+1)**-1
+    total += probabilities[i]
+#probabilities[0] = 1
+probabilities[:] = [x/total for x in probabilities]
+result = weighted_values(values, probabilities, 10**5)
 
 
-
-
-
+#print(result)    
+#plt.rcParams.update({'figure.figsize':(7,5), 'figure.dpi':100})
+plt.hist(result, bins=20) #different bin
+plt.gca().set(title='T = 10', ylabel='Frequency');
